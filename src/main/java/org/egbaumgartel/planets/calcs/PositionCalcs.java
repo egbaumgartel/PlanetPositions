a345349cfeafd8841a69df2ce650ab4b9a831732
egbaumgartel <greg.baumgartel@gmail.com>
2017-03-15 10:39:03 -0600

    Placeholder class for calculations, may split later.

---
+++ src/main/java/org/egbaumgartel/planets/calcs/PositionCalcs.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.egbaumgartel.planets.calcs;
+
+/**
+ * @author gregb
+ *
+ * @date 2017-03-15
+ *
+ */
+public class PositionCalcs {
+	
+	/*
+	 * Placeholder currently, some useful URL's
+	 * 
+	 * http://www.jgiesen.de/kepler/kepler.html
+	 * 
+	 * http://www.petesqbsite.com/sections/tutorials/tuts/ppcomp.html
+	 * 
+	 * ftp://ssd.jpl.nasa.gov/pub/eph/planets/ioms/ExplSupplChap8.pdf
+	 * 
+	 */
+
+}
